dd15e53ef64128ce86360dd620dc87fd213e0098
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2025-04-25 23:40:58 +0530

    feat: Add Open Graph meta tags for link previews

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,7 +64,17 @@ export default defineNuxtConfig({
           name: 'description', 
           content: 'Personal portfolio website of Ayush Jaipuriar, a Software Engineer showcasing experience in backend, frontend, DevOps, and cloud technologies.' 
         },
-        { name: 'format-detection', content: 'telephone=no' }
+        { name: 'format-detection', content: 'telephone=no' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://ayush-jaipuriar.github.io/Personal-Portfolio/' },
+        { property: 'og:title', content: 'Ayush Jaipuriar | Software Engineer' },
+        { property: 'og:description', content: 'Personal portfolio website of Ayush Jaipuriar, a Software Engineer.' },
+        { property: 'og:image', content: 'https://ayush-jaipuriar.github.io/Personal-Portfolio/ayush-jaipuriar.jpeg' },
+        { property: 'twitter:card', content: 'summary_large_image' },
+        { property: 'twitter:url', content: 'https://ayush-jaipuriar.github.io/Personal-Portfolio/' },
+        { property: 'twitter:title', content: 'Ayush Jaipuriar | Software Engineer' },
+        { property: 'twitter:description', content: 'Personal portfolio website of Ayush Jaipuriar, a Software Engineer.' },
+        { property: 'twitter:image', content: 'https://ayush-jaipuriar.github.io/Personal-Portfolio/ayush-jaipuriar.jpeg' },
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
