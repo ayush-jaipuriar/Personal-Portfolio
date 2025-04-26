7fb0d34e97520ffdd136e3be38d40e724c5d7886
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2025-04-26 13:54:50 +0530

    feat: Add additional favicon and apple touch icon links to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -78,6 +78,9 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { 
           rel: 'stylesheet', 
           href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap' 
